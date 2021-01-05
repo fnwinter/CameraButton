466c1825717ee0b8851b8243c58fa3c6cdcbc4d6
JungJik Lee <jungjik.lee@samsung.com>
2021-01-06 00:54:25 +0900

    imple timer
    timer

--- source/CameraButton.py
+++ source/CameraButton.py
@@ -1,4 +1,8 @@
 import tkinter as tk
+import subprocess
+import time
+import threading
+import logging
 
 from ppadb.client import Client as AdbClient
 
@@ -10,6 +14,9 @@ class CameraButton(tk.Frame):
         self.columnconfigure(0, weight = 1)
         self.remote_connection = tk.IntVar()
         self.create_widgets()
+        self.client = None
+        self.device = None
+        self.timer = None
 
     def create_widgets(self):
         self.connectButton = tk.Button(self)
@@ -44,19 +51,60 @@ class CameraButton(tk.Frame):
         self.stopButton.grid(row=3, column=2, sticky = tk.W + tk.E)
 
     def connection(self):
-        print("connect")
-        print(self.remote_connection.get())
+        if self.remote_connection.get():
+            # remote connect
+            print("remote connect")
+            subprocess.check_output("adb usb")
+            time.sleep(3)
+            self.client = AdbClient()
+            self.device = self.client.devices()[0]
+            address = self.get_ip_addr()
+
+            subprocess.check_output("adb tcpip 5678")
+            time.sleep(5)
+            self.client = AdbClient("127.0.0.1", 5037)
+            self.client.remote_connect(address, 5678)
+            print(self.client.devices())
+            self.device = self.client.devices()[1]
+        else:
+            print("connect")
+            self.client = AdbClient()
+            self.device = self.client.devices()[0]
+
+    def kill_server(self):
+        subprocess.check_output("adb kill-server")
 
     def capture(self):
         print("capture")
+        if self.device:
+            self.device.input_keyevent("KEYCODE_CAMERA")
+        if self.timer:
+            self.start()
 
     def start(self):
-        print("start")
+        timer_tick = int(self.timeEntry.get())
+        self.timer = threading.Timer(timer_tick, self.capture)
+        self.timer.start()
 
     def stop(self):
         print("stop")
+        if self.timer:
+            self.timer.cancel()
+            self.timer = None
+
+    def get_ip_addr(self):
+        if self.device:
+            address = self.device.shell("ip addr show wlan0")
+            find_inet = address.find("inet ")
+            last_slash = address.find('/', find_inet)
+            ip = address[find_inet+5: last_slash]
+            print(ip)
+            return ip
+        return ""
 
 if __name__ == "__main__":
+    logging.getLogger("ppadb").setLevel(logging.DEBUG)
+
     root = tk.Tk()
     root.title("Android Camera Button")
     root.geometry("320x100+100+100")
